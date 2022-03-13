4c51a49dbf5fbec1f74734d1c3534b3d7ec55d07
renatomak <renato.mark.silva@gmail.com>
2022-03-13 11:09:12 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 import { ReactComponent as Arrow } from "assets/img/arrow.svg";
 import { MoviePage } from "types/movie";
@@ -8,30 +8,15 @@ type Props = {
   data: MoviePage
 }
 const Pagination = ({ setPage, data }: Props) => {
-  const [disabledPrevious, setDisablePrevious] = useState(true);
 
-  const next = () => {
-    const nextPage = data.number + 1;
-    setPage(nextPage >= data.totalPages ? data.number : nextPage)
-    setDisablePrevious(false)
-  }
-  
-  const previous = () => {
-    const previousPage = data.number;
-
-    setPage(previousPage > 0 ? previousPage -1 : 0)
-
-    previousPage < 2 && setDisablePrevious(true);
-
-  }
   return (
     <div className="dsmovie-pagination-container">
       <div className="dsmovie-pagination-box">
-        <button className="dsmovie-pagination-button" disabled={disabledPrevious} onClick={previous}>
+        <button className="dsmovie-pagination-button" disabled={data.first} onClick={() => setPage(data.number - 1)}>
           <Arrow />
         </button>
         <p>{`${data.number+1} de ${data.totalPages}`}</p>
-        <button className="dsmovie-pagination-button" disabled={false} onClick={next}>
+        <button className="dsmovie-pagination-button" disabled={data.last} onClick={() => setPage(data.number + 1)}>
           <Arrow className="dsmovie-flip-horizontal" />
         </button>
       </div>
