b8e2cec9133a4486ddee301a30d853a96deae22a
renatomak <renato.mark.silva@gmail.com>
2022-03-13 11:35:44 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -7,7 +7,19 @@ import "./style.css";
 type Props = {
   movieId: string;
 };
+type User = {
+  movieId: number,
+  email: string,
+  score: number
+}
+const initialStateUser = {
+  movieId: 0,
+  email: '',
+  score: 1
+}
 const FormCard = ({ movieId }: Props) => {
+  const [user, setUser] = useState<User>(initialStateUser);
+
   const [movie, setMovie] = useState({
     id: 0,
     title: "",
@@ -19,6 +31,18 @@ const FormCard = ({ movieId }: Props) => {
   const getMovie = async () => {
     const response = await axios.get(`${BASE_URL}/movies/${movieId}`);
     setMovie(response.data);
+  }
+
+  const handleOnChange = ({ target }: any) => {
+    const { name, value } = target;
+    const id = parseInt(movieId, 10)
+    setUser({...user, [name]: value, movieId: id })
+    console.log(user)
+  }
+
+  const save = async () => {
+    const response = await axios.put(`${BASE_URL}/scores`, {...user})
+    console.log(response)
   }
 
   useEffect(() => {
@@ -37,11 +61,18 @@ const FormCard = ({ movieId }: Props) => {
         <form className="dsmovie-form">
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              onChange={handleOnChange}
+              name="email"
+              value={user.email}
+            />
           </div>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
+            <select className="form-control" id="score" onChange={handleOnChange} name="score" value={user.score}>
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -50,7 +81,7 @@ const FormCard = ({ movieId }: Props) => {
             </select>
           </div>
           <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">
+            <button type="button" className="btn btn-primary dsmovie-btn" onClick={save}>
               Salvar
             </button>
           </div>
